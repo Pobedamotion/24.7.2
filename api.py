68d2bd4d69cd7a4dc0992087dcec7812559d168f
Viktoriia Zagorodnikova <134017362+viktoriia-zagorodnikova@users.noreply.github.com>
2026-07-13 15:44:09 -0500

    Update api.py

--- api.py
+++ api.py
@@ -6,7 +6,7 @@ class PetFriends:
         self.base_url = 'https://petfriends.skillfactory.ru/'
 
     def get_api_key(self, email: str, password: str):
-        """Метод получает API ключ по email и паролю"""
+        """Gets an API key using email and password"""
         headers = {
             'email': email,
             'password': password
@@ -20,7 +20,7 @@ class PetFriends:
         return status, result
 
     def get_list_of_pets(self, auth_key: dict, filter: str = ''):
-        """Метод получает список питомцев"""
+        """Gets the list of pets"""
         headers = {'auth_key': auth_key['key']}
         filter = {'filter': filter}
         res = requests.get(self.base_url + 'api/pets',
@@ -34,7 +34,7 @@ class PetFriends:
 
     def add_new_pet(self, auth_key: dict, name: str,
                     animal_type: str, age: str, pet_photo: str):
-        """Метод добавляет нового питомца"""
+        """Adds a new pet"""
         headers = {'auth_key': auth_key['key']}
         data = {
             'name': name,
@@ -52,7 +52,7 @@ class PetFriends:
         return status, result
 
     def delete_pet(self, auth_key: dict, pet_id: str):
-        """Метод удаляет питомца по ID"""
+        """Deletes a pet by ID"""
         headers = {'auth_key': auth_key['key']}
         res = requests.delete(self.base_url + 'api/pets/' + pet_id,
                              headers=headers)
@@ -65,7 +65,7 @@ class PetFriends:
 
     def update_pet_info(self, auth_key: dict, pet_id: str, name: str,
                         animal_type: str, age: str):
-        """Метод обновляет информацию о питомце"""
+        """Updates a pet's information"""
         headers = {'auth_key': auth_key['key']}
         data = {
             'name': name,
@@ -83,7 +83,7 @@ class PetFriends:
 
     def add_new_pet_without_photo(self, auth_key: dict, name: str,
                                   animal_type: str, age: str):
-        """Метод добавляет питомца без фото"""
+        """Adds a pet without a photo"""
         headers = {'auth_key': auth_key['key']}
         data = {
             'name': name,
@@ -100,7 +100,7 @@ class PetFriends:
         return status, result
 
     def add_photo_of_pet(self, auth_key: dict, pet_id: str, pet_photo: str):
-        """Метод добавляет фото к питомцу"""
+        """Adds a photo to a pet"""
         headers = {'auth_key': auth_key['key']}
         file = {'pet_photo': (pet_photo, open(pet_photo, 'rb'), 'image/jpeg')}
         res = requests.post(self.base_url + 'api/pets/set_photo/' + pet_id,
